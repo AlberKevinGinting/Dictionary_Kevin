e940d6e85f1530fe9137bc4629a77c87bf05d59e
AlberKevinGinting <163402204+AlberKevinGinting@users.noreply.github.com>
2024-03-14 17:41:08 +0700

    Update and rename README.md to README.py
    Penerjemah Kata kata modern

---
+++ README.py
@@ -0,0 +1,10 @@
+meme_dict = {
+            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
+            "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
+            "OTW": "Saat seseorang di dalam perjalanan kesesuatu tempat",
+            }
+word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
+if word in meme_dict.keys():
+    print("Makna kata", word, "adalah", meme_dict[word])
+else:
+    print("Makna kata tidak ditemukan")
